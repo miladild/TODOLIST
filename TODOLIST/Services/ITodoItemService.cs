8be8c1d2747ede0bed550a7d3cc21836a97ccee0
miladild <miladild@hotmail.com>
2020-07-07 21:37:29 +1200

    Add project files.

---
+++ TODOLIST/Services/ITodoItemService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TODOLIST.Models;
+
+namespace TODOLIST.Services
+{
+    public interface ITodoItemService
+    {
+        Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync();
+    }
+}
---
+++ TODOLIST/ViewModels/TodoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TODOLIST.Models;
+
+namespace TODOLIST.ViewModels
+{
+    public class TodoViewModel
+    {
+        public IEnumerable<TodoItem> Items { get; set; }
+    }
+}
