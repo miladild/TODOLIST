05b0052cd03afcd56d8bf1d9e83f83ed963379fd
miladild <miladild@hotmail.com>
2020-07-08 22:37:02 +1200

    ManageUser controller added

---
+++ TODOLIST/Controllers/ManageUsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TODOLIST.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public ManageUsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
